47a73a96accc2eb6ea0f00150f88d51d2ca81a0e
Saad Taimoor <saadtaimoor8@gmail.com>
2021-02-28 18:44:46 -0500

    Implemented Topics

--- AlMualim/Controllers/TopicsController.cs
+++ AlMualim/Controllers/TopicsController.cs
@@ -25,129 +25,22 @@ namespace AlMualim.Controllers
             return View(await _context.Topics.ToListAsync());
         }
 
-        // GET: Topics/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: Topics/Notes/ID
+        public async Task<IActionResult> Notes(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            // Get notes list
+            var notes = await _context.Notes.Where(n => n.Topics.Any(t => t.ID == id)).OrderBy(n => n.Surah).ToListAsync();
 
-            var topics = await _context.Topics
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (topics == null)
-            {
-                return NotFound();
-            }
+            // Get topic metadata
+            var topic = await _context.Topics.FirstOrDefaultAsync(t => t.ID == id);
+            ViewData["Topic"] = topic;
 
-            return View(topics);
-        }
+            // Get surah metadata
+            var surah = await _context.Surah.ToListAsync();
+            ViewData["Surah"] = surah;
 
-        // GET: Topics/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Topics/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Title")] Topics topics)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(topics);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(topics);
-        }
-
-        // GET: Topics/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var topics = await _context.Topics.FindAsync(id);
-            if (topics == null)
-            {
-                return NotFound();
-            }
-            return View(topics);
-        }
-
-        // POST: Topics/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Title")] Topics topics)
-        {
-            if (id != topics.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(topics);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TopicsExists(topics.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(topics);
-        }
-
-        // GET: Topics/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var topics = await _context.Topics
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (topics == null)
-            {
-                return NotFound();
-            }
-
-            return View(topics);
-        }
-
-        // POST: Topics/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var topics = await _context.Topics.FindAsync(id);
-            _context.Topics.Remove(topics);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool TopicsExists(int id)
-        {
-            return _context.Topics.Any(e => e.ID == id);
+            // Filter notes by 
+            return View(notes);
         }
     }
 }
