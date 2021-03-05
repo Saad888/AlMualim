fc58d9455aecffdbcec92031a420e460e48e83f7
Saad Taimoor <saadtaimoor8@gmail.com>
2021-03-04 20:31:04 -0500

    added about page

---
+++ AlMualim/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AlMualim.Data;
+using AlMualim.Models;
+
+namespace AlMualim.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly AlMualimDbContext _context;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
